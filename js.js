86a6f0fdb3fce7d6c0be99c6ba87c34508b66042
ultraShazikk <shaxzodbekkhayrullayevich>
2023-11-12 09:58:01 +0500

    js dars 1

---
+++ js.js
@@ -0,0 +1,49 @@
+var Name = String(prompt("Ismingizni yozing..."))
+console.log(Name);
+
+var Familiya = String(prompt("Familiyangizni yozing"))
+console.log(Familiya);
+
+var Age = Number(prompt("Yoshingizni yozing..."))
+console.log(Age);
+
+var Insof = String(prompt("Insof yes"))
+console.log(Insof);
+
+var Fakt = prompt("Skruch haqiqiy hayotda sizmi")
+console.log(Fakt);
+
+var Muddat = String(prompt("Amerikaga qachon safar qilmoqchisiz"))
+console.log(Muddat);
+
+var Diqqat = String(prompt("Apple kampaniyasini sotib olganingizni tan olasizmi"))
+console.log(Diqqat);
+
+var Meva = String(prompt("Eng yoqtirgan mevangiz {tishlangan bo'lsa ham} "))
+console.log(Meva);
+
+var Miqdor = Number(prompt("Dunyo bo'ylab qancha uyingiz bor"))
+console.log(Miqdor);
+
+var Aql = String(prompt("Boy bo'lish siri nimada"))
+console.log(Aql);
+
+var Sps = String(prompt("Yesli cho SPS"))
+
+var Javob = [
+    "Ism: " + " "+ Name + "\n" +
+    "Familiya: " + " "+ Familiya + "\n" +
+    "Yosh: " + " "+ Age + "\n" +
+    "To'y qachon: " + " "+ Maqsad + "\n" +
+    "Qachon: " + " "+ Fakt + "\n" +
+    "Jami: " + " "+ Muddat + "\n" +
+    "Fakt: " + " "+ Diqqat + "\n" +
+    "Meva: " + " "+ Meva + "\n" +
+    "Jami: " + " "+ Miqdor + "\n" +
+    "Aql: " + " "+ Aql + "\n" 
+
+]
+
+alert(Javob)
+
+
